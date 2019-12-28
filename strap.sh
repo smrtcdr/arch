f4121218db7414456a1e6329dd0a364557895c93
smrtcdr <33865644+smrtcdr@users.noreply.github.com>
2019-12-28 21:36:14 +0200

    Update strap.sh

--- strap.sh
+++ strap.sh
@@ -122,8 +122,8 @@ cat <<-EOF >> ${TARGET_DIR}/root/.bashrc
 # my stuff
 export PS1='\[\033[1;36m\]\u\[\033[1;31m\]@\[\033[1;32m\]\h:\[\033[1;35m\]\w\[\033[1;31m\]\$\[\033[0m\] '
 EOF
-mkdir -p /${TARGET_DIR}/root/arch
-cp ./strap.sh /root
+mkdir -p ${TARGET_DIR}/root/arch
+cp ./strap.sh /root/arch
 du -hsx ${TARGET_DIR}
 sync;
 
